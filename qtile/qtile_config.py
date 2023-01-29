c2a7afb7f7a717ad95f8dcf4950aeb80d9488fe2
Artur <artur.abelian0@gmail.com>
2023-01-29 17:30:21 +0300

    Update config

--- qtile/qtile_config.py
+++ qtile/qtile_config.py
@@ -1,12 +1,12 @@
 from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-from libqtile.utils import guess_terminal
+
 
 
 
 mod = "mod4"
-terminal = guess_terminal()
+terminal = 'alacritty'
 
 #######
 # KEY #
